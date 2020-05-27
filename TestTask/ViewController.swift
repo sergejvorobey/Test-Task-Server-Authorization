5640151e856eab5cba62218cac3cc6f9e1ede72e
sergeyvorobey <phanta2408@yandex.ru>
2020-05-27 14:09:54 +0300

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Sergey Vorobey on 27/05/2020.
+//  Copyright © 2020 Сергей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
